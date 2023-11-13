5d0c2c306408fbe27fce9e37999fa31d5d7e4da8
Gabriel Nii Amarh Ashitey <niiamarhgabriel@gmail.com>
2023-11-13 10:40:20 +0000

    styling the ui

--- src/Components/AllCountries/AllCountries.jsx
+++ src/Components/AllCountries/AllCountries.jsx
@@ -10,7 +10,7 @@ const AllCountries = () => {
     const getAllCountries = async() => {
 
         try {
-          const res = await fetch('${apiURL}/all'); 
+          const res = await fetch(`${apiURL}/all`); 
           
             if(!res.ok) throw new error('Oops! Something went wrong!');
 
@@ -22,10 +22,8 @@ const AllCountries = () => {
 
             setIsLoading(false);
         } catch (error) {
-
             setIsLoading(false);
-            setError(error.message);
-            
+            setError(error.message);  
         }
     };
 
@@ -39,7 +37,25 @@ const AllCountries = () => {
       </div>
 
       <div className="country_bottom">
+        {isLoading && !error && <h4>Loading...........</h4>}
+        {error && !isLoading && <h4>{error}</h4>}
 
+        {
+            countries?.map(country => (
+                <div className="country_card">
+                    <div className="country_img">
+                        <img src={country.flags.png} alt="flag of a country" />
+                    </div>
+
+                    <div className="country_data">
+                        <h3>{country.name.common}</h3>
+                        <h6> Population: {country.population}</h6>
+                        <h6> Region: {country.region}</h6>
+                        <h6> Capital: {country.capital}</h6>
+                    </div>
+                </div>
+            ))
+        }
       </div>
     </div>
   
